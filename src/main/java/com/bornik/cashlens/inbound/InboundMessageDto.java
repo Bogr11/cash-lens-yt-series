14caa4c2a96b6be6beed656e76cc9ce62f02d63e
borys <borysnikolenko@gmail.com>
2026-08-10 15:48:41 +0200

    clean up

--- src/main/java/com/bornik/cashlens/inbound/InboundMessageDto.java
+++ src/main/java/com/bornik/cashlens/inbound/InboundMessageDto.java
@@ -2,10 +2,6 @@ package com.bornik.cashlens.inbound;
 
 public record InboundMessageDto(InboundMessage inboundMessage) {
 
-    public String externalId() {
-        return inboundMessage.getExternalId();
-    }
-
     public String payload() {
         return inboundMessage.getPayload();
     }
